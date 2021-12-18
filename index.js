1463dd28812a7c918311f100889bb3c1422f7761
Eysa Azhar <89656526+bhattieysa@users.noreply.github.com>
2021-12-18 15:56:45 +0500

    Curd Operations with nodejs

--- index.js
+++ index.js
@@ -17,18 +17,18 @@ const pool = mysql.createPool({
 
 })
 
-//Select all data
+//Select all data from database
 app.get('/select', function (req, res) {
     pool.getConnection((err, connection) => {
         if (err) throw err
         console.log(`connection as id ${connection.threadId}`)
         connection.query('SELECT * FROM employee', (err, rows, fields) => {
             connection.release // return the connect to pool
-            if (!err) 
+            if (!err)
                 res.send(rows)
             else
                 console.log(err)
-    })
+        })
     })
 })
 //select particular data using where
@@ -36,13 +36,13 @@ app.get('/select:id', function (req, res) {
     pool.getConnection((err, connection) => {
         if (err) throw err
         console.log(`connection as id ${connection.threadId}`)
-        connection.query('SELECT * FROM employee WHERE empID= ?',[req.params.id], (err, rows, fields) => {
+        connection.query('SELECT * FROM employee WHERE empID= ?', [req.params.id], (err, rows, fields) => {
             connection.release // return the connect to pool
-            if (!err) 
+            if (!err)
                 res.send(rows)
             else
                 console.log(err)
-    })
+        })
     })
 })
 // delete particular data
@@ -50,13 +50,13 @@ app.delete('/delete:id', function (req, res) {
     pool.getConnection((err, connection) => {
         if (err) throw err
         console.log(`connection as id ${connection.threadId}`)
-        connection.query('DELETE  FROM employee WHERE empID= ?',[req.params.id], (err, rows, fields) => {
+        connection.query('DELETE  FROM employee WHERE empID= ?', [req.params.id], (err, rows, fields) => {
             connection.release // return the connect to pool
-            if (!err) 
+            if (!err)
                 res.send(`Employee with employee id:${req.params.id}has been removed`)
             else
                 console.log(err)
-    })
+        })
     })
 })
 // Add  data
@@ -64,19 +64,19 @@ app.post('/add', function (req, res) {
     pool.getConnection((err, connection) => {
         if (err) throw err
         console.log(`connection as id ${connection.threadId}`)
-const params=req.body
+        const params = req.body
 
 
-        connection.query('INSERT INTO employee SET ?',params, (err, rows, fields) => {
+        connection.query('INSERT INTO employee SET ?', params, (err, rows, fields) => {
             connection.release // return the connect to pool
-            if (!err) 
+            if (!err)
                 res.send(`Employee with employee name:${params.Name}has been added`)
             else
                 console.log(err)
+        })
     })
-    })
- 
-console.log(req.body)
+
+    console.log(req.body)
 
 })
 //Update The Data
@@ -85,19 +85,19 @@ app.put('/update', function (req, res) {
         if (err) throw err
         console.log(`connection as id ${connection.threadId}`)
 
-const {empID,Name,EmpCode,Salary}=req.body 
+        const { empID, Name, EmpCode, Salary } = req.body
 
 
-        connection.query('UPDATE employee SET name=? WHERE empID = ?',[Name,empID], (err, rows, fields) => {
+        connection.query('UPDATE employee SET name=? WHERE empID = ?', [Name, empID], (err, rows, fields) => {
             connection.release // return the connect to pool
-            if (!err) 
+            if (!err)
                 res.send(`Employee with employee name:${Name}has been added`)
             else
                 console.log(err)
-    })
+        })
     })
 
-console.log(req.body)
+    console.log(req.body)
 
 })
 app.listen(port, () => console.log(`Express Server is running at port no ${port}`))
